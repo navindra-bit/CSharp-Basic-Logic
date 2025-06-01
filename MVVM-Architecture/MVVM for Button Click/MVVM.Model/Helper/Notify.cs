19faf361bb78b4762d8bcb674926e142263da16e
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-06-01 14:10:05 +0530

    MVVM Button Click

---
+++ MVVM-Architecture/MVVM for Button Click/MVVM.Model/Helper/Notify.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVM.Model.Helper
+{
+    public class Notify : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void onPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

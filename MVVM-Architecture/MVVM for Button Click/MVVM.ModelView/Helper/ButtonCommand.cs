835608ca6804b2ade6919a6e6df9abff1b8a58ae
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-06-01 14:13:24 +0530

    MVVM Button Update1

---
+++ MVVM-Architecture/MVVM for Button Click/MVVM.ModelView/Helper/ButtonCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM.ModelView.Helper
+{
+    public class ButtonCommand : ICommand
+    {
+        private Action _action;
+        public ButtonCommand ( Action action )
+        {
+            _action = action;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _action.Invoke();
+        }
+    }
+}

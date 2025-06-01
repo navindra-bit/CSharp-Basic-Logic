2c23fed1ffed2ce0e9df1f61c6a8960e7b190705
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-06-01 14:16:37 +0530

    MVVM Button helper

---
+++ MVVM-Architecture/MVVM for Button Click/MVVM.View/Helper/NoRadioValueConvert.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MVVM.View.Helper
+{
+    public class NoRadioValueConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value == null)return false;
+             if (value.ToString().ToUpper() == "NO") return true;
+            else return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolvvalue)
+                return boolvvalue ? "No" : "Yes";
+            return "Yes";
+        }
+    }
+}

8992dfd4f2a5810b5a52d24c71c972824b685d4d
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-06-01 14:11:30 +0530

    MVVM Button Update

---
+++ MVVM-Architecture/MVVM for Button Click/MVVM.Model/Models/ViewModels.cs
@@ -0,0 +1,47 @@
+﻿using MVVM.Model.Helper;
+
+namespace MVVM.Model.Models
+{
+    public class ViewModels : Notify
+    {
+        public ViewModels()
+        {
+            _isValid = "";
+            _status = "";
+            _color = "";
+        }
+
+        private string _isValid;
+        public string isValid
+        {
+            get => _isValid;
+            set
+            {
+                _isValid = value;
+                onPropertyChanged();
+                
+            }
+        }
+        private string _status;
+        public string status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                onPropertyChanged();
+            }
+        }
+        private string _color;
+        public string color
+        {
+            get => _color;
+            set
+            {
+                _color = value;
+                onPropertyChanged();
+            }
+        }
+    }
+    
+}

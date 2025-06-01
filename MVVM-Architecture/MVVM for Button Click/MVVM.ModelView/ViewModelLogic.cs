588f7bde1f10cba9a232888780b624ac4dd101bf
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-06-01 14:14:08 +0530

    MVVM Button Update

---
+++ MVVM-Architecture/MVVM for Button Click/MVVM.ModelView/ViewModelLogic.cs
@@ -0,0 +1,32 @@
+﻿using MVVM.Model.Models;
+using MVVM.ModelView.Helper;
+
+namespace MVVM.ModelView
+{
+    public class ViewModelLogic
+    {
+        public ButtonCommand ButtonCommand { get; set; }
+
+        public ViewModels ViewModels { get; set; }
+        public ViewModelLogic()
+        {
+            ButtonCommand = new ButtonCommand(Regiter);
+            ViewModels = new ViewModels();
+            ViewModels.isValid = "Yes";
+        }
+        public void Regiter()
+        {
+            if(ViewModels.isValid.ToUpper() == "YES")
+            {
+                ViewModels.status = "Registration successful";
+                ViewModels.color = "Green";
+            }
+            else
+            {
+                ViewModels.status = "Registration failed";
+                ViewModels.color = "Red";
+            }
+        }
+
+    }
+}

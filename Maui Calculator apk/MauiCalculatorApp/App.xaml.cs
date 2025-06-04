6f7c3a87105aa26f20a15f9694005edce509a85d
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-06-04 21:58:14 +0530

    Maui calc Apk app xaml

---
+++ Maui Calculator apk/MauiCalculatorApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiCalculatorApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
